e1ece549892cbeb74621c875f0d2c52f8545bc93
David Weidenbenner <44938475+comic-man@users.noreply.github.com>
2021-11-08 17:21:09 -0600

    k

--- pokemon/src/app/app.module.ts
+++ pokemon/src/app/app.module.ts
@@ -4,22 +4,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
-import { PokemonComponent } from './pokemon/pokemon.component';
-import { PokemonNameComponent } from './pokemon-name/pokemon-name.component';
-import { PokemonServicesComponent } from './pokemon-services/pokemon-services.component';
-import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    PokemonNameComponent,
-    PokemonServicesComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    HttpClientModule
+    HttpClientModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
