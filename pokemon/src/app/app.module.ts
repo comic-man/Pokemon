54c40f974a8b621ed4e4db8ac510eb7204970151
David Weidenbenner <44938475+comic-man@users.noreply.github.com>
2021-11-09 00:28:10 -0600

    d

--- pokemon/src/app/app.module.ts
+++ pokemon/src/app/app.module.ts
@@ -5,11 +5,15 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { HttpClientModule } from '@angular/common/http';
+import { NameComponent } from './name/name.component';
+import { PowersComponent } from './powers/powers.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
+    NameComponent,
+    PowersComponent,
   ],
   imports: [
     BrowserModule,
